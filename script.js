f7cf806df681f5565c64a520f3bd1515008e41f6
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-15 16:35:15 +0600

    project completed

--- script.js
+++ script.js
@@ -11,8 +11,34 @@ for (let i = 0; i < cards.length; i++) {
         const p = document.createElement('p');
         p.innerText = titleCount + ' ' + title;
         titleContainer.appendChild(p);
-        titleCount++ ;
+        titleCount++;
         totalPrice += price;
         document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
     })
 }
+
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click', function () {
+    const couponElement = document.getElementById('input-field').value;
+    const couponCode = couponElement.split(' ').join('').toUpperCase();
+    console.log(couponCode);
+    if (totalPrice >= 200) {
+        if (couponCode === 'SELL200') {
+            const discountElement = document.getElementById('discountPrice');
+            const discountAmount = totalPrice * 0.2;
+            discountElement.innerText = discountAmount.toFixed(2);
+            // Rest total   
+            const restTotal = document.getElementById('total');
+            restTotal.innerText = totalPrice - discountAmount.toFixed(2);
+            document.getElementById('input-field').value = '';
+        }
+        else {
+            alert("Invalid Coupon");
+            document.getElementById('input-field').value = '';
+        }
+    }
+    else {
+        alert("Please purchase at least $200");
+        document.getElementById('input-field').value = '';
+    }
+})
